74abd2cd630cdb420052d442682119c89ea12626
Ryan Lok <rlok.pc@gmail.com>
2024-02-03 23:32:02 -0800

    Implemented logic for fetching song data and write to db

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import {createClient} from '@supabase/supabase-js'
 import 'dotenv/config'
+import * as data from './info.json' assert {type: "json"} 
 
 const supabase = createClient(process.env.DB_url, process.env.DB_anon);
-
-//get access token 
 
 main()
 
 async function main() {
     const access_token = await getAccessToken()
 
+    const info = data.default
+    console.log(info)
+    console.log(info.length)
     console.log(access_token)
+    fetchTopFives(info, access_token.access_token)
 }
 
 
@@ -29,16 +32,44 @@ async function getAccessToken() {
     } catch {
         console.log("Failed to fetch access token")
     }
-    
 } 
 
+async function fetchTopFives(countryList, access_token) {
+    
+    const bulkRows = []
 
-// console.log(markets.json())
+    for (let i = 0; i < countryList.length; i++) {
+        
+        if (countryList[i].playlist_id != null) {
+            const playlist_id = countryList[i].playlist_id
+            const songData = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks?limit=5`, {
+                headers:  {
+                    Authorization: `Bearer ${access_token}`
+                }
+            })
+            .then(response => response.json()
+            .then(json => {return json}))
 
-// const data = await fetch("https://api.spotify.com/v1/playlists/37i9dQZEVXbKj23U1GF4IR/tracks?market=CA", {
-//     headers: {
-//         Authorization: `Bearer ${access_token}`
-//     }
-// })
+            const {items} = songData;
+            console.log(items)
+            const row = {
+                "country": countryList[i].country,
+                "songone": items[0],
+                "songtwo": items[1],
+                "songthree": items[2],
+                "songfour": items[3],
+                "songfive": items[4]
+            }
 
-// console.log(data.json())
+            bulkRows.push(row)
+        } else {
+            continue
+        }
+    }
+
+    const { error } = await supabase
+        .from('history')
+        .insert(bulkRows)
+
+}
+
